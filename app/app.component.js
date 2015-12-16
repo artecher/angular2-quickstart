cb565ecf95cda3297268cf74c341b7b8f08d3809
ethan2014 <ethan.lee@sparkventures.co.nz>
2015-12-16 13:45:09 +1300

    First commit

---
+++ app/app.component.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1) {
+    var AppComponent;
+    return {
+        setters:[],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.title = 'Tour of Heroes';
+                    this.hero = 'Windstorm';
+                }
+                return AppComponent;
+            })();
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+})
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+export class AppComponent {
+  public title = 'Tour of Heroes';
+  public hero = 'Windstorm';
+}
